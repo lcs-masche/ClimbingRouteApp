c743c82e57153279a10d559c928d1ae00d24eefa
Moritz Asche <moritz@asches.de>
2021-03-18 08:44:23 -0400

    Initial Commit

---
+++ ClimbingRouteApp/ClimbingRouteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimbingRouteAppApp.swift
+//  ClimbingRouteApp
+//
+//  Created by Mo Asche on 2021-03-18.
+//
+
+import SwiftUI
+
+@main
+struct ClimbingRouteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimbingRouteApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClimbingRouteApp
+//
+//  Created by Mo Asche on 2021-03-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
